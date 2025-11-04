00edcccfbe4c1d2b6397b74fe525edebd0bc9303
Methul Jayasuriya <methullakvindu5@gmail.com>
2025-11-05 02:06:41 +0530

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import sitemap from 'vite-plugin-sitemap';
 
+
 export default defineConfig({
   plugins: [
     react(),
